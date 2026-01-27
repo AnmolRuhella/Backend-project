9c24a5606ca7ae6acd304491b056f103273ae590
AnmolRuhella <anmolruhella23@gmail.com>
2026-01-28 00:07:30 +0530

    complete mongoose model

--- models/hospital-management/doctor.models.js
+++ models/hospital-management/doctor.models.js
@@ -1,7 +1,42 @@
 
 import mongoose from "mongoose"
 
+const hospitalHours = new mongoose.Schema(
+    {
+        timeInhours : {
+            type : Number,
+            required : true 
+          },
+          hospital :{
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "Hospital"
+          }
+    },{timestamps:true
+    }
+)
+
 const doctorSchema = new mongoose.Schema({
+    name:{
+        type : String,
+        required : true 
+    },
+    salary :{
+        type : String,
+        requied : true
+    },
+    qualification : {
+        type : String,
+        required : true 
+    },
+    experience : {
+        type : Number,
+        default : 0
+    },
+    worksInHospitals :
+    {
+        type : [hospitalHours],
+    }
+
 
 },{timestamps : true})
 
--- models/hospital-management/hospital.model.js
+++ models/hospital-management/hospital.model.js
@@ -1,8 +1,33 @@
+import mongoose from "mongoose";
 
-import mongoose from "mongoose"
+const hospitalSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    addressLine1: {
+      type: String,
+      requied: true,
+    },
+    addressLine1: {
+      type: String,
+    },
+    city: {
+      type: String,
+      requied: true,
+    },
+    pincode: {
+      type: String,
+      requied: true,
+    },
+    specalizedIn :[
+        {
+        type : String
+        }
+    ]
+  },
+  { timestamps: true },
+);
 
-const hospitalSchema = new mongoose.Schema({
-
-},{timestamps : true})
-
-export const Hospital  = mongoose.model("Hospital",hospitalSchema )
+export const Hospital = mongoose.model("Hospital", hospitalSchema);
